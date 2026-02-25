2cd1953790d077e6e445d30b48bdc265a5b5c875
Diki Naufal <diki.naufal29dev@gmail.com>
2026-02-26 06:46:09 +0700

    Menambahkan jawaban tugas 2 (Cleaning Wrong Format)

--- latihan_soal1.10medium.py
+++ latihan_soal1.10medium.py
@@ -9,6 +9,18 @@ print(df.duplicated(subset=['Pekerjaan', 'Pengalaman_Tahun', 'Tanggal_Isi']).sum
 # B
 df.drop_duplicates(subset=['Pekerjaan', 'Pengalaman_Tahun', 'Tanggal_Isi'], inplace=True)
 
+# 2. Cleaning Wrong Format (Multiple Columns)
+# A
+df['Pengalaman_Tahun'] = df['Pengalaman_Tahun'].str.replace(' thn', '')
 
+# B
+df['Gaji_Bulanan_Juta'] = df['Gaji_Bulanan_Juta'].str.replace('Rp ', '')
+
+# C
+df['Pengalaman_Tahun'] = df['Pengalaman_Tahun'].astype(float)
+df['Gaji_Bulanan_Juta'] = df['Gaji_Bulanan_Juta'].astype(float)
+
+# D 
+df['Tanggal_Isi'] = pd.to_datetime(df['Tanggal_Isi'], format='mixed', dayfirst=True)
 
 print(df.to_string())
