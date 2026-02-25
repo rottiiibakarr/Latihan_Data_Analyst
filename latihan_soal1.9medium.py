2a5aecc4120b219f0c725b81c04e2960ad7c213f
Diki Naufal <diki.naufal29dev@gmail.com>
2026-02-25 20:38:09 +0700

    Menambahkan jawaban tugas 3 (Cleaning Wrong Data)

--- latihan_soal1.9medium.py
+++ latihan_soal1.9medium.py
@@ -21,5 +21,14 @@ df['Harga_Satuan'] = df['Harga_Satuan'].astype(float)
 # C
 df['Tanggal_Masuk'] = pd.to_datetime(df['Tanggal_Masuk'], format='mixed', dayfirst=True)
 
+# 3. Cleaning Wrong Data (Typo Teks & Spasi)
+# A
+df['Kategori'] = df['Kategori'].str.upper()
+
+# B
+df['Nama_Barang'] = df['Nama_Barang'].str.strip() # Untuk menghapus whitespace atau spasi
+
+
+
 
 print(df.to_string())
