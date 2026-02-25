cdefb76b9f221abb9a174fa82f6439d2e03e942f
Diki Naufal <diki.naufal29dev@gmail.com>
2026-02-25 17:57:48 +0700

    Menambahkan jawaban tugas 2 (Cleaning Wrong Format)

--- latihan_soal1.9medium.py
+++ latihan_soal1.9medium.py
@@ -13,6 +13,13 @@ df.drop_duplicates(inplace=True)
 
 # 2. Cleaning Wrong Format
 # A
-df['Harga_Satuan']
+df['Harga_Satuan'] = df['Harga_Satuan'].str.replace('USD ', '')
+
+# B
+df['Harga_Satuan'] = df['Harga_Satuan'].astype(float)
+
+# C
+df['Tanggal_Masuk'] = pd.to_datetime(df['Tanggal_Masuk'], format='mixed', dayfirst=True)
+
 
 print(df.to_string())
