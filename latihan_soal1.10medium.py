281c2c900ade6260685133c2e96377532cacd34f
Diki Naufal <diki.naufal29dev@gmail.com>
2026-02-26 06:29:11 +0700

    Menambahkan jawaban tugas 1 (Removing Duplicates)

--- latihan_soal1.10medium.py
+++ latihan_soal1.10medium.py
@@ -2,3 +2,13 @@ import pandas as pd
 
 df = pd.read_csv('data_nilai1.10medium.csv')
 
+# 1. Removing Duplicates
+# A
+print(df.duplicated(subset=['Pekerjaan', 'Pengalaman_Tahun', 'Tanggal_Isi']).sum())
+
+# B
+df.drop_duplicates(subset=['Pekerjaan', 'Pengalaman_Tahun', 'Tanggal_Isi'], inplace=True)
+
+
+
+print(df.to_string())
