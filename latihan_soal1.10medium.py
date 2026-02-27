10c3383341a485f4cb434266b1ce57874ba11277
Diki Naufal <diki.naufal29dev@gmail.com>
2026-02-27 16:17:54 +0700

    Menambahkan jawaban semua tugas

--- latihan_soal1.10medium.py
+++ latihan_soal1.10medium.py
@@ -31,4 +31,59 @@ df['Pekerjaan'] = df['Pekerjaan'].str.title()
 df['Kota_Domisili'] = df['Kota_Domisili'].str.strip().str.title()
 
 
+# 5. Cleaning Empty Cells (Logika Domain):
+# A 
+maksimal_pengalaman_tahun = 40
+median_pengalaman_tahun = df.loc[df['Pengalaman_Tahun'] <= maksimal_pengalaman_tahun, 'Pengalaman_Tahun'].median()
+
+df.fillna({'Pengalaman_Tahun': median_pengalaman_tahun}, inplace=True)
+
+# B
+mean_gaji_bulanan = df['Gaji_Bulanan_Juta'].mean()
+df.fillna({'Gaji_Bulanan_Juta': mean_gaji_bulanan}, inplace=True)
+
+"""
+Penjelasan:
+Untuk tugas 5 saya dahulukan terlebih dahulu dari tugas 4, karena pada tugas 5 saya harus mengisi
+nilai kosong (NaN) tersebut dengan nilai median, lalu diisi dengan kolom 'Pengalaman_Tahun' dengan
+variabel 'median_penglaman_tahun', kalo tidak didahulukan, perhitungannya akan tidak sesuai.
+"""
+
+# 4. Cleaning Wrong Data (Logical Error & Outlier):
+# A
+df['Pengalaman_Tahun'] = df['Pengalaman_Tahun'].abs()
+
+# B
+df.loc[df['Pengalaman_Tahun'] > maksimal_pengalaman_tahun, 'Pengalaman_Tahun'] = median_pengalaman_tahun
+
+# 6. Cleaning Data (Final):
+# A 
+df = df.reset_index(drop=True)
+
+# B
+print(df.info())
+"""
+Output:
+<class 'pandas.core.frame.DataFrame'>
+RangeIndex: 6 entries, 0 to 5
+Data columns (total 6 columns):
+ #   Column             Non-Null Count  Dtype
+---  ------             --------------  -----
+ 0   ID_Survei          6 non-null      object
+ 1   Pekerjaan          6 non-null      object
+ 2   Pengalaman_Tahun   6 non-null      float64
+ 3   Gaji_Bulanan_Juta  6 non-null      float64
+ 4   Kota_Domisili      6 non-null      object
+ 5   Tanggal_Isi        6 non-null      datetime64[ns]
+dtypes: datetime64[ns](1), float64(2), object(3)
+memory usage: 420.0+ bytes
+None
+"""
+
 print(df.to_string())
+
+"""
+Catatan:
+Untuk tugas 5A, untuk pengalaman tahun tidak saya konversi tipe data ke int ya, karena saya memenuhi 
+tugas 2C, dengan mempertahankan tipe data float.
+"""
