2e1da6f3b149084c253c6a0bba2730f6c5357656
Diki Naufal <diki.naufal29dev@gmail.com>
2026-02-28 22:00:17 +0700

    Menambahkan jawaban tugas 4 (Cleaning Wrong Data: Logika & Outlier) - jawaban 6 (Cleaning Data: Final)

--- latihan_soal1.11medium.py
+++ latihan_soal1.11medium.py
@@ -8,7 +8,7 @@ df.drop_duplicates(subset=['Nama_Calon', 'Jalur_Masuk', 'Skor_Ujian', 'Tanggal_L
 
 # 2. Cleaning Wrong Format: 
 # A
-df['Tinggi_Badan_cm'] = df['Tinggi_Badan_cm'].str.replace(' cm', '')
+df['Tinggi_Badan_cm'] = pd.to_numeric(df['Tinggi_Badan_cm'].str.replace(' cm', '')) 
 
 # B
 df['Tanggal_Lahir'] = pd.to_datetime(df['Tanggal_Lahir'], format='mixed', dayfirst=True)
@@ -19,5 +19,38 @@ df['Nama_Calon'] = df['Nama_Calon'].str.title()
 
 # B 
 df['Jalur_Masuk'] = df['Jalur_Masuk'].str.upper()
+
+
+# 4. Cleaning Wrong Data (Logika & Outlier):
+# A 
+df['Skor_Ujian'] = df['Skor_Ujian'].abs()
+
+# B
+maksimal_tinggi_badan = 220 
+mean_tinggi_badan = df.loc[df['Tinggi_Badan_cm'] <= maksimal_tinggi_badan, 'Tinggi_Badan_cm'].mean()
+df.loc[df['Tinggi_Badan_cm'] > maksimal_tinggi_badan, 'Tinggi_Badan_cm'] = mean_tinggi_badan
+
+# 5. Cleaning Empty Cells:
+# A
+median_skor_ujian = df['Skor_Ujian'].median()
+df.fillna({'Skor_Ujian': median_skor_ujian}, inplace=True)
+
+# B 
+mean_tinggi_badan = df.loc[df['Tinggi_Badan_cm'] <= maksimal_tinggi_badan, 'Tinggi_Badan_cm'].mean()
+"""
+Disini saya memanggil lagi 'mean_tinggi_badan', kenapa? 
+
+Karena hasil dari nilai yang lebih besar dari 'maksimal_tinggi_badan' itu saya isi dengan
+'mean_tinggi_badan' awal, yang dimana saya ganti dulu nilai yang lebih besar dari 'maksimal_tinggi_badan'
+dengan 'mean_tinggi_badan', lalu setelah diganti, saya panggi lagi variabel 
+mean_tinggi_badan = df['Tinggi_Badan_cm'].mean() tersebut, karena nilai yang lebih besar dari
+'maksimal_tinggi_badan' sudah diganti dengan yang baru, yaitu 'mean_tinggi_badan', lalu setelah
+diganti, lakukan mean ulang secara kesuluruhan (termasuk yang baru) untuk mengisi nilai yang kosong (NaN)
+dengan mean (rata-rata).
+"""
+df.fillna({'Tinggi_Badan_cm': mean_tinggi_badan}, inplace=True)
+
+# 6. Cleaning Data (Final): 
+df = df.reset_index(drop=True)
 
 print(df.to_string())
